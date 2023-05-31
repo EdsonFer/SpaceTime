24afae0db71c97907087af445f903125acb3544a
Edson <edlivelifehd1z@gmail.com>
2023-05-30 23:21:10 -0300

    teste branch

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-900 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo aqui</h1>
     </div>
   )
 }
