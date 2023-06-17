46980a58d46f597d5697785c9483143fe88e5bec
Edson <edlivelifehd1z@gmail.com>
2023-06-17 17:52:00 -0300

    server aula4

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,39 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
-import fastify from "fastify";
-import cors from "@fastify/cors"
-import jwt from '@fastify/jwt'
-import { memoriesRoutes } from "./routes/memories";
+import fastify from 'fastify';
+import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
+import { memoriesRoutes } from './routes/memories';
 import { authRoutes } from './routes/auth';
+import multipart from '@fastify/multipart';
+import { uploadRoutes } from './routes/upload';
+import { resolve } from 'path';
 
 const app = fastify();
 
+app.register(multipart);
+
+app.register(require('fastify-static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+});
+
 app.register(cors, {
   origin: true,
-})
+});
 
 app.register(jwt, {
   secret: 'spaceTime',
-})
+});
 
-app.register(authRoutes)
-app.register(memoriesRoutes)
+app.register(authRoutes);
+app.register(uploadRoutes);
+app.register(memoriesRoutes);
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("Server is running on port 3333💕😁👍👍");
+    console.log('Server is running on port 3333💕😁👍👍');
   });
